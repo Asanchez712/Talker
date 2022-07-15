d451269aff6acfda29b88813d6053bad6570c213
Alexander <asanchez3315@gmail.com>
2022-07-14 23:49:53 -0400

    initial commit

---
+++ public-html/db-conn.inc.php
@@ -0,0 +1,21 @@
+<?php
+
+require 'db-pswd.inc.php';
+
+ try {
+
+     //DOCKER
+     $connection = new PDO('mysql:host=mysql;dbname=talker_db', DOCKER[0], DOCKER[1]);
+
+
+     print "Success! Connected to the database!";
+
+ } catch (PDOException $e) {
+     print "Error!: " . $e->getMessage() . "<br/>";
+     die();
+ }
+
+ 
+
+
+?>
---
+++ public-html/db-connection.php
@@ -0,0 +1,3 @@
+<?php
+require'db-conn.inc.php';
+?>
