434ab7d609dfa67115bb25c5c1c661ba8c788e1b
Asanchez712 <109328228+Asanchez712@users.noreply.github.com>
2022-07-16 17:27:31 -0400

    sign up form

--- public-html/index.php
+++ public-html/index.php
@@ -10,7 +10,39 @@
 <!-- CSS only -->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">  </head>
   <body>
-    <h1>TALKER</h1>
+  <div class="container">
+	<div class="row justify-content-md-center">
+		<div class="col-12 col-md-auto"><h1>TALKER | SIGN UP</h1></div>
+	</div>
+
+	<hr><br>
+
+	<div class="row">
+		<div class="col-6">
+			<form>
+				<div class="form-group">
+					<label for="formSignUpEmail">Email address</label>
+					<input type="email" class="form-control" id="formSignUpEmail" placeholder="Enter your email address">
+				</div>
+				<div class="form-group">
+					<label for="formSignUpPassword">Password</label>
+					<input type="password" class="form-control" id="formSignUpPassword" placeholder="Enter your password">
+
+					<input type="password" class="form-control mt-4" id="formSignUpPasswordConf" placeholder="Confirm your password">
+				</div>
+				<button style='margin-top:20px' type="submit" class="btn btn-primary">Submit</button>
+			</form>
+		</div>
+
+		<div class="col-6">
+			<p>Hello and welcome to Talker! We are very happy that you want to join our great community!</p>
+			<p>Please, enter your email and password. Your must have access to your email because we will send
+          a confirmation code to that address. Your password must be between 8 and 16 characters long, with at
+          least one uppercase and one lowercase character, one number and one special character (@, *, $ or #).</p>
+			<p>We hope you'll enjoy Talker!</p>
+		</div>
+	</div>
+</div>
 
     <!-- Optional Javascript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
