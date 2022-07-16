594c5d673b56634cf55d5830c98313f0c439b16d
Asanchez712 <109328228+Asanchez712@users.noreply.github.com>
2022-07-16 16:28:01 -0400

    Bootstrap starter template

--- public-html/index.php
+++ public-html/index.php
@@ -1,10 +1,21 @@
-<!DOCTYPE html>  
-<head>  
- <title>Hello World!</title>
-</head>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>TALKER</title>
+    
+    <!-- Bootstrap CSS -->
+<!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">  </head>
+  <body>
+    <h1>TALKER</h1>
 
-<body>  
- <h1>Hello World!</h1>
- <p><?php echo 'We are running PHP, version: ' . phpversion(); ?></p>
- <p><?php phpinfo(); ?></p>
-</body>
+    <!-- Optional Javascript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<!-- JavaScript Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>    
+  </body>
+</html>
